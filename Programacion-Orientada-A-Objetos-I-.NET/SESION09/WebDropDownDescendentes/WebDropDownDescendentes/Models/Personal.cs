571d59ce98870beb88951ee2a7de9c8f97518a03
Ronald Jaime <160687644+Ronald-jsd@users.noreply.github.com>
2025-02-24 10:28:52 -0500

    Add files via upload

---
+++ Programacion-Orientada-A-Objetos-I-.NET/SESION09/WebDropDownDescendentes/WebDropDownDescendentes/Models/Personal.cs
@@ -0,0 +1,18 @@
+﻿namespace WebDropDownDescendentes.Models
+{
+    public class Personal
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string DNI { get; set; }
+
+        //clave foranea
+        public int DistritoId { get; set; }
+
+        //propiedad de navegacion
+        //clase personal se esta relacionando con distrito
+        public Distrito Distrito { get; set; }
+
+
+    }
+}
