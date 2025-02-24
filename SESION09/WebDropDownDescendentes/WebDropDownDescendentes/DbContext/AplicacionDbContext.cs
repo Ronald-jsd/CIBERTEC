56dd69beadf39fdfd3a8bf7778203400dd224580
Ronald Jaime <160687644+Ronald-jsd@users.noreply.github.com>
2025-02-24 10:24:18 -0500

    Add files via upload

---
+++ SESION09/WebDropDownDescendentes/WebDropDownDescendentes/DbContext/AplicacionDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using WebDropDownDescendentes.Models;
+
+
+namespace WebDropDownDescendentes.DbContext
+
+{
+    public class AplicacionDbContext : DbContext
+    {
+        public AplicacionDbContext(DbContextOptions<AplicacionDbContext> options)<AplicacionDbContext> Options) : base(options){}
+
+        //aqui van las clase del modelo
+    public DbSet <Provincia> Provincia {  get; set; }
+    public DbSet<Provincia> Provincia { get; set; }
+
+}
+}
---
+++ SESION09/WebDropDownDescendentes/WebDropDownDescendentes/Models/Provincia.cs
@@ -0,0 +1,11 @@
+﻿namespace WebDropDownDescendentes.Models
+{
+    public class Provincia
+    {
+        public int Id { get; set; } 
+        public string Nombre { get; set; }
+
+        //Establenciendo cardinalidad uno a muchos con Distrito
+        public ICollection<Distrito> Distrito { get; set; }
+    }
+}
