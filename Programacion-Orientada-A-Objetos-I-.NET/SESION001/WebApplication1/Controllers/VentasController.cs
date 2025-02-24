474f3d6ea90eb1a13265abb6faf21dba1b3f07c2
Ronald Jaime <160687644+Ronald-jsd@users.noreply.github.com>
2025-02-24 11:47:03 -0500

    Add files via upload

---
+++ Programacion-Orientada-A-Objetos-I-.NET/SESION001/WebApplication1/Controllers/VentasController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models; //Importamos la carpeta de nuestro modelo(Donde estan los atributos y metodos)****
+
+namespace WebApplication1.Controllers
+{
+    /*
+     * La clase VentasController hereda de la clase Controller.
+     * En ASP.NET Core, un controlador maneja solicitudes HTTP, interactúa con el modelo, 
+     * y devuelve una respuesta (vista HTML, JSON, etc.).
+     */
+    public class VentasController : Controller
+    {
+        /*
+         * Este método maneja solicitudes GET. Cuando un usuario accede a la URL correspondiente,
+         * devuelve la vista Index.cshtml y pasa un nuevo objeto Venta a la vista.
+         *
+         * - public: El método es accesible desde cualquier parte de la aplicación.
+         * - IActionResult: Indica que el método puede devolver diferentes tipos de respuestas (View, Json, etc.).
+         * - return View(new Venta()): Devuelve la vista Index con un nuevo objeto Venta.
+         */
+        public IActionResult Index()
+        {
+            return View(new Venta());
+        }
+
+        /*
+         * Este método maneja solicitudes POST. Se utiliza para procesar datos enviados desde la vista (por ejemplo, un formulario).
+         * - [HttpPost]: Indica que este método responde a solicitudes HTTP POST.
+         * - IActionResult Index(Venta v): Recibe el objeto Venta que contiene los datos del formulario.
+         */
+        //Agregamos lo siguiente para calcular
+        [HttpPost]
+        public IActionResult Index(Venta v)
+        {
+            // Utilizamos ViewBag para pasar los valores calculados a la vista.
+            ViewBag.descuento = v.Descuento().ToString("####.#0");
+            ViewBag.neto = v.Neto().ToString("####.#0");
+
+            // Devolvemos la vista Index.cshtml con el objeto Venta y los valores calculados.
+            return View(v);
+        }
+    }
+}
---
+++ Programacion-Orientada-A-Objetos-I-.NET/SESION001/WebApplication1/Models/Venta.cs
@@ -0,0 +1,72 @@
+﻿/*
+ Manejo de Clases y Objetos
+
+La empresa Tapicería “FAST” nos pide realizar un programa que permita registrar la venta de
+muebles que la empresa ofrece a los clientes.
+
+En este proceso debemos ingresar el número de la boleta, la fecha, el nombre del cliente, la
+descripción del mueble, y su precio. La empresa aplica el siguiente descuento: si el precio es
+mayor a 500 se aplica un descuento que representa al 10% del precio, sino el descuento es el
+2% del precio. El precio de venta neto es la diferencia del precio y su descuento.
+
+Se pide realizar las siguientes actividades:
+ Crear un proyecto ASP.NET en Visual C#.
+ Diseño de la clase, sus atributos, propiedades y métodos.
+ Diseñar una vista definiendo sus controles.
+ Programe el formulario instanciando la clase, ingrese sus datos y visualice los resultados.
+ */
+
+
+// nombreDelProyecto.Models 
+namespace WebApplication1.Models
+{
+    
+    public class Venta
+    {
+        //atributos de la clase
+        //prop  + tab
+        public string NroBoleta { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Nombre { get; set; }
+        public string Mueble { get; set; }
+        public double precio { get; set; }
+
+
+        //constructor para inicializar los atributos
+        public Venta()
+        {
+            NroBoleta = "";
+           // Fecha = DateTime.Now;// devuelve fecha y hora
+            Fecha = DateTime.Today; // devuelve solo la fecha 
+            Nombre = "";
+            Mueble = "";
+            precio = 0;  
+
+        }
+
+        //Metodos de la clase
+            //Metodo calcular el valor del descuento
+        public double Descuento()
+        {
+            if(precio == 0)
+            {
+                return 0;
+            }   else if (precio > 500)
+            {
+                return precio * 0.1;
+            }   else
+            {
+                return precio * 0.02;
+            }
+        }
+
+            //Metodo calcular el precio neto de venta
+            public double Neto()
+        {
+            return precio - Descuento();
+        }
+
+
+
+    }
+}
